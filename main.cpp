8a0c20b8ce131803e549870cbffc58b5887156d4
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2022-04-14 18:33:24 -0500

    ready and pending half implement

--- main.cpp
+++ main.cpp
@@ -3,67 +3,69 @@
 #include <algorithm>
 using namespace std;
 
+
+
 class Task {
-    public:
-        int id;
-        int period;
-        int arrivalTime;
-        int executionTime;
-        int deadline = arrivalTime+period;
-        bool missedDeadline = false;
+public:
+    int id;
+    int period;
+    int arrivalTime;
+    int executionTime;
+    int absoluteDeadline = arrivalTime + period;
+    bool missedDeadline = false;
+    //constructor needs to initialzie deadline to arrivaltime+period. might need a isPeriodic boolean for Task
 };
 
+
 class Processor {
-    public:
-        Task* activeTask;
-        vector<Task*> pendingTaskPool;
-        vector<Task*> readyTaskPool;
-        int simulationStep;
-        bool step() {
-            simulationStep++;
-            activeTask->executionTime = activeTask->executionTime - 1;
-            
-            //move tasks that need to arrive into readyTaskPool
-            for(int i = 0; i < pendingTaskPool.size(); i++)
-            {
-                if(pendingTaskPool[i]->arrivalTime == simulationStep)
-                {
-                    vector<Task*>::iterator position = find(pendingTaskPool.begin(), pendingTaskPool.end(), pendingTaskPool[i]);
-                    if(position != pendingTaskPool.end())
-                    {
-                        readyTaskPool.insert(readyTaskPool.end(), std::make_move_iterator(position), std::make_move_iterator(position));
-                    }
-                }
-            }
+public:
+    Task* activeTask;
+    vector<Task*> pendingTaskPool; //may need to move these pools to multiprocessor
+    vector<Task*> readyTaskPool;
+    int simulationStep;
+    bool step() {
 
-            //move done task from readdyTaskPool
-            if(activeTask->executionTime == 0)
+        //move tasks that need to arrive into readyTaskPool
+        for (int i = 0; i < pendingTaskPool.size(); i++)
+        {
+            if (pendingTaskPool[i]->arrivalTime == simulationStep)
             {
-                vector<Task*>::iterator position = find(readyTaskPool.begin(), readyTaskPool.end(), activeTask);
-                if(position != readyTaskPool.end())
-                {
-                    readyTaskPool.erase(position);
-                }
-            }
-
-            if(!readyTaskPool.empty() && !pendingTaskPool.empty())
-            {
-                activeTask = readyTaskPool[0];
-                //this is FIFO for now, this is where i need to decide the next activeTask
-
-                for(int i = 0; i < readyTaskPool.size(); i++)
-                {
-                    cout<< readyTaskPool[i]->id << ": "<<readyTaskPool[i]->executionTime<<endl;
-                }
-                cout<<endl;
-            }
-            else
-            {
-                return true;
-            }
-
-            return false;
+                readyTaskPool.push_back(pendingTaskPool[i]);
+;           }
         }
+
+        activeTask->executionTime = activeTask->executionTime - 1;
+        simulationStep++;
+
+        //remove done task from readyTaskPool if execution is finished
+        if (activeTask->executionTime == 0)
+        {
+            vector<Task*>::iterator position = find(readyTaskPool.begin(), readyTaskPool.end(), activeTask);
+            if (position != readyTaskPool.end())
+            {
+                readyTaskPool.erase(position);
+            }
+        }
+
+        if (!readyTaskPool.empty() && !pendingTaskPool.empty())
+        {
+            activeTask = readyTaskPool[0];
+            //this is FIFO for now, this is where i need to decide the next activeTask
+
+            for (int i = 0; i < readyTaskPool.size(); i++)
+            {
+                cout << readyTaskPool[i]->id << ": " << readyTaskPool[i]->executionTime << endl;
+            }
+            cout << endl;
+        }
+        else
+        {
+            return true;
+        }
+
+        return false;
+    }
+    //constructor here, initialize activeTask using a scheduler (can a scheduler be a callback function for Processor?)
 };
 
 int main()
@@ -83,14 +85,21 @@ int main()
     task3.executionTime = 4;
     task3.id = 666;
 
+    Task task4;
+    task3.arrivalTime = 10;
+    task3.executionTime = 8;
+    task3.id = 64;
+
     Processor intel;
     intel.pendingTaskPool.push_back(&task1);
     intel.pendingTaskPool.push_back(&task2);
-    intel.activeTask = intel.readyTaskPool[0];
+    intel.pendingTaskPool.push_back(&task3);
+    intel.pendingTaskPool.push_back(&task4);
+    intel.activeTask = &task1;
 
-    while(!intel.step())
+    while (!intel.step())
     {
-        cout<<"STEP"<<endl;
+        cout << "STEP" << endl;
     }
 
     return 0;
